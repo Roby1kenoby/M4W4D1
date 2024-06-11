777b67d86338c4db3be447cc60c8c90066f4f631
Roby1kenoby <r0by1kenoby@hotmail.it>
2024-06-11 23:49:51 +0200

    added backend page skeleton plus started working on modal form

--- server/serverScript.js
+++ server/serverScript.js
@@ -1,22 +1,10 @@
 
 let urlServer = 'https://striveschool-api.herokuapp.com/api/product/'
 let tokenAuth = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjY3M2M5NTdmNmI0YjAwMTU0MjhmYmIiLCJpYXQiOjE3MTgwNDE3NDksImV4cCI6MTcxOTI1MTM0OX0.cWXhY0IiDf_t2ETumzY24fHKfUkwU7xaKt_sXZeu7Bw"
-let testProduct = {
-    name: "nomeProdotto6",
-    description: "descProdotto2",
-    brand: "brandProdotto2",
-    imageUrl: "imageProdotto2",
-    price: 100
-}
 
-// function to authenticate 
-let auth = async function(){
-    let keyData = await fetch(urlAuthentication)
-    let data = await keyData.json()
-    console.log(data)
-}
+let products = []
 
-// function to add products
+// function to add products to the backend
 let addProduct = async function(product){
     try {
         const resp = await fetch(urlServer, {
@@ -28,19 +16,73 @@ let addProduct = async function(product){
             }
         })
         if (resp.status >= 400 && resp.status <= 600){
-            const data = await resp.text()
+            const data = await resp.text()  
             alert(`Warning, product not saved for the following reason:\n${data}`)
         }
         else{
-            alert(`Product correctly inserted. Status resp: ${data.status}`)
+            alert(`Product correctly inserted. Status resp: ${resp.status}`)
         }
     } catch (error) {
         console.log('sono in error: ', error)
     }
-    
-
 }
+
+// function to get a specific product via it's id, or all the products if ther's no id
+let getProducts = async function(id=''){
+    try {
+        const resp = await fetch(urlServer+id,{
+            method: 'GET',
+            headers: {
+                'Authorization': tokenAuth,
+            }
+        })
+        if (resp.status >= 400 && resp.status <= 600){
+            alert('Warning, product not found')
+        }
+        else{
+            let data = await resp.json()    
+            console.log(data)
+            return data
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// function to delete a product by it's id
+let deleteProduct = async function(id){
+    try {
+        const resp = await fetch(urlServer+id,{
+            method: 'DELETE',
+            headers:{
+                "Authorization": tokenAuth
+            }
+        })
+    } catch (error) {
+        
+    }
+}
+
+// function to update a product by it's id
+let updateProduct = async function(id, bod){
+    try {
+        const resp = await fetch(urlServer+id,{
+            method: "PUT",
+            headers: {
+                "Authorization": tokenAuth,
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(bod)
+
+        })
+        console.log(resp)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
 // fastload
 onload = (e) => {
-    addProduct(testProduct)
+    getProducts()    
 }
